bffcc02d01f177653361e65351ce4a5a1ca88fc6
Duong Nguyen <duongital@gmail.com>
2023-02-07 20:36:12 +0700

    chore: git ignore vscode

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-// getting from related module files
-pub mod a_standard_io;
 pub mod a_module;
+pub mod a_standard_io;
 
 // main is root function and used only in file `main.rs`
 fn main() {
     // a_standard_io::print_me();
     a_module::draw_me();
+    a_module::draw_me();
+    println!("Hello world");
+    a_standard_io::print_me();
 }
