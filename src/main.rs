a06381936db90e39c37d9f686e8ac56bdce14ef3
Duong Nguyen <duongital@gmail.com>
2023-02-27 08:59:08 +0700

    chore: refactoring files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 pub mod a_module;
-pub mod a_standard_io;
 
 // main is root function and used only in file `main.rs`
 fn main() {
-    // a_standard_io::print_me();
     a_module::draw_me();
-    a_module::draw_me();
+
     println!("Hello world");
-    a_standard_io::print_me();
 }
